a601b093198960456349a36292a2f4f84410db90
Mikekhoi <147293279+Mikekhoi@users.noreply.github.com>
2024-03-17 11:31:09 +0700

    1.6

---
+++ js/signin.js
@@ -0,0 +1,44 @@
+import { 
+    getAuth, 
+    signInWithEmailAndPassword,
+} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
+import Toasty from "./toast.js";
+
+const auth = getAuth();
+const signInEmail = document.getElementById("signin-email");
+const signInPass = document.getElementById("signin-password");
+const signInBtn = document.getElementById("signin-btn");
+const toastHTMLElement = document.getElementById("toast");
+const toastContent = document.getElementById("toast-content");
+console.log(auth)
+
+
+signInBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    var emailValueSI = signInEmail.value;
+    var passwordValueSI = signInPass.value;
+
+    const signInToast = new Toasty(toastHTMLElement, toastContent);
+    if(emailValueSI.trim().length == 0 || passwordValueSI.trim().length == 0)
+    {
+        signInToast.showAlert("Không để trống", 'red');
+    }
+    else {
+        signInWithEmailAndPassword(auth, emailValueSI, passwordValueSI)
+        .then((useCredential) => {
+            const user = useCredential.user;
+            console.log(user);
+            async function signIn() {
+            signInToast.showAlert("Đăng nhập thành công!", 'green');
+            await new Promise(resolve => setTimeout(resolve, 1000))
+            window.location.href = "index.html";
+            }
+            signIn()
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            signInToast.showAlert(errorMessage, 'red');
+        })
+    }
+})
