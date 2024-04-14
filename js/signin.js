03af03e8ab6e95db1845d74afb8a65dbd85c0335
randomguy100408 <147293466+randomguy100408@users.noreply.github.com>
2024-04-14 10:23:58 +0700

    fix login js function

--- js/signin.js
+++ js/signin.js
@@ -1,54 +1,5 @@
-
 import {
     getAuth,
-    signInWithEmailAndPassword,
-    updateProfile
-  } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
-import Toasty from "./toast.js";
-
-import { 
-    getAuth, 
-    signInWithEmailAndPassword,
-} from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
-import Toasty from "./toast.js";
-
- c89e418eccc88df34d53316bcf03be8f826340d2
- HEAD
-console.log(auth);
-console.log(auth);
-
-
-signInBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    var emailValueSI = signInEmail.value;
-    var passwordValueSI = signInPass.value;
-
-    const signInToast = new Toasty(toastHTMLElement, toastContent);
-    if(emailValueSI.trim().length == 0 || passwordValueSI.trim().length == 0)
-    {
-        signInToast.showAlert("Không được để trống!", 'red');
-    }
-    else {
-        signInWithEmailAndPassword(auth, emailValueSI, passwordValueSI)
-        .then((useCredential) => {
-            const user = useCredential.user;
-            console.log(user);
-            async function signIn() {
-            signInToast.showAlert("Đăng nhập thành công!", 'green');
-            await new Promise(resolve => setTimeout(resolve, 1000))
-            window.location.href ="index.html";
-            }
-            signIn()
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            signInToast.showAlert(errorMessage, 'red')
-        })
-    }
-})
-import { 
-    getAuth, 
     signInWithEmailAndPassword,
 } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
 import Toasty from "./toast.js";
@@ -61,9 +12,6 @@ const toastHTMLElement = document.getElementById("toast");
 const toastContent = document.getElementById("toast-content");
 console.log(auth)
 
- c89e418eccc88df34d53316bcf03be8f826340d2
-console.log(auth)
-
 
 signInBtn.addEventListener("click", (e) => {
     e.preventDefault();
@@ -71,27 +19,26 @@ signInBtn.addEventListener("click", (e) => {
     var passwordValueSI = signInPass.value;
 
     const signInToast = new Toasty(toastHTMLElement, toastContent);
-    if(emailValueSI.trim().length == 0 || passwordValueSI.trim().length == 0)
-    {
+    if (emailValueSI.trim().length == 0 || passwordValueSI.trim().length == 0) {
         signInToast.showAlert("Không được để trống!", 'red');
         signInToast.showAlert("Không để trống", 'red');
     }
     else {
         signInWithEmailAndPassword(auth, emailValueSI, passwordValueSI)
-        .then((useCredential) => {
-            const user = useCredential.user;
-            console.log(user);
-            async function signIn() {
-            signInToast.showAlert("Đăng nhập thành công!", 'green');
-            await new Promise(resolve => setTimeout(resolve, 1000))
-            window.location.href = "index.html";
-            }
-            signIn()
-        })
-        .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            signInToast.showAlert(errorMessage, 'red')
-        })
+            .then((useCredential) => {
+                const user = useCredential.user;
+                console.log(user);
+                async function signIn() {
+                    signInToast.showAlert("Đăng nhập thành công!", 'green');
+                    await new Promise(resolve => setTimeout(resolve, 1000))
+                    window.location.href = "index.html";
+                }
+                signIn()
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                signInToast.showAlert(errorMessage, 'red')
+            })
     }
 })
