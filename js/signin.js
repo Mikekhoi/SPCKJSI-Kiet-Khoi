fd626c0b2d3def30c7866b9b1e5947bbd392a7da
giabao1511 <giabao712411@gmail.com>
2024-05-26 00:29:25 +0700

    fix bug proj

--- js/signin.js
+++ js/signin.js
@@ -7,25 +7,22 @@ const auth = getAuth();
 const signInEmail = document.getElementById("signin-email");
 const signInPass = document.getElementById("signin-password");
 const signInBtn = document.getElementById("signin-btn");
-const toastHTMLElement = document.getElementById("toast");
-const toastContent = document.getElementById("toast-content");
 
 signInBtn.addEventListener("click", (e) => {
   e.preventDefault();
   var emailValueSI = signInEmail.value;
   var passwordValueSI = signInPass.value;
 
-  const signInToast = new Toasty(toastHTMLElement, toastContent);
   if (emailValueSI.trim().length == 0 || passwordValueSI.trim().length == 0) {
     alert("Không để trống");
   } else {
     signInWithEmailAndPassword(auth, emailValueSI, passwordValueSI)
       .then((useCredential) => {
         const user = useCredential.user;
-        console.log(user);
+        localStorage.setItem("currentUser", user.email);
+
         async function signIn() {
-          signInToast.showAlert("Đăng nhập thành công!", "green");
-          await new Promise((resolve) => setTimeout(resolve, 1000));
+          alert("Login successfully");
           window.location.href = "index.html";
         }
         signIn();
@@ -33,7 +30,7 @@ signInBtn.addEventListener("click", (e) => {
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
-        alert(errorMessage, "red");
+        alert(errorMessage);
       });
   }
 });
--- js/signup.js
+++ js/signup.js
@@ -1,9 +1,8 @@
 import {
   getAuth,
   createUserWithEmailAndPassword,
-  updateProfile
+  updateProfile,
 } from "https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js";
-
 
 const auth = getAuth();
 const signUpEmail = document.getElementById("signup-email");
@@ -11,8 +10,7 @@ const signUpUsername = document.getElementById("signup-username");
 const signUpPass = document.getElementById("signup-password");
 const signUpConfirmPassword = document.getElementById("signup-confirm");
 const signUpBtn = document.getElementById("signup-btn");
-const toastHTMLElement = document.getElementById("toast");
-const toastContent = document.getElementById("toast-content");
+
 signUpBtn.addEventListener("click", (e) => {
   e.preventDefault();
   var emailValueSU = signUpEmail.value;
@@ -21,39 +19,35 @@ signUpBtn.addEventListener("click", (e) => {
   let vietThuong = /[a-z]/g;
   let vietHoa = /[A-Z]/g;
   let chuSo = /[0-9]/g;
-  const signUpToast = new Toasty(toastHTMLElement, toastContent);
   if (
     signUpUsername.value.trim().length == 0 ||
     signUpEmail.value.trim().length == 0 ||
     signUpPass.value.trim().length == 0 ||
     signUpConfirmPassword.value.trim().length == 0
   ) {
-    alert("Something still empty :(")
+    alert("Something still empty :(");
   } else if (signUpPass.value.trim().length < 8) {
-    alert("Password must be at least 8 characters :(")
+    alert("Password must be at least 8 characters :(");
   } else if (!signUpPass.value.trim().match(vietThuong)) {
-    alert("Password must have at least 1 lowercase character :(")
+    alert("Password must have at least 1 lowercase character :(");
   } else if (!signUpPass.value.trim().match(vietHoa)) {
-    alert("Password must have at least 1 uppercase character :(")
+    alert("Password must have at least 1 uppercase character :(");
   } else if (!signUpPass.value.trim().match(chuSo)) {
-    alert("Password must have at least 1 alphanumeric character :(")
+    alert("Password must have at least 1 alphanumeric character :(");
   } else if (signUpPass.value.trim() != signUpConfirmPassword.value.trim()) {
-    alert("Password isn't match :(")
+    alert("Password isn't match :(");
   } else {
     createUserWithEmailAndPassword(auth, emailValueSU, passwordValueSU)
       .then((userCredential) => {
         // Signed up
         const user = userCredential.user;
-        alert("Sign up sucessfully")
-        window.location.href = "./signin.html"
-        // ...
+        alert("Sign up sucessfully");
+        window.location.href = "./signin.html";
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
         alert(errorMessage);
-        // ..
       });
   }
 });
-
